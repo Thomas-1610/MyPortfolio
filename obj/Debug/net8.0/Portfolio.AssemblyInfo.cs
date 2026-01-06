a1919f475a2999b91a901f787e2a91ba8f779df7
Thomas-1610 <thomaslcl.1610@gmail.com>
2026-01-06 19:08:14 -0300

    some chenges

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b470d98ce87583c5115880a8b363197853565cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dabc88e2882459d934ce47621593a9f7bc6b340")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
