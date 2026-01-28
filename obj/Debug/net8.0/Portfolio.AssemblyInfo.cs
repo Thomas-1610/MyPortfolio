85bb554d7bab46283d1237fd665bf8c547e3bf9f
Thomas-1610 <thomaslcl.1610@gmail.com>
2026-01-27 23:51:42 -0300

    sla mn, nem sei mais

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd146ee8526c9a16e8e39bbc30c8ff2d79838b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58217e6ea7ff8e782f42a5a00b01f73acc356a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
